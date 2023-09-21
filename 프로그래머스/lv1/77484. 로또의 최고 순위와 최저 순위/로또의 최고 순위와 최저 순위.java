f969d1bd05e9075198b3313f96368f5affc779a8
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-09-21 23:54:57 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-		int min = 60;
+		int max = 0;
         
 		for(int i= 0; i < 6; i++) {
-			if(lottos[i] < min) {
-				min = lottos[i]; // 최소값 찾기
+			if(lottos[i] > max) {
+				max = lottos[i]; // 최대값 찾기
 			}
 		}
-		if(min == 0) {
+		if(max == 0) { // 모두 0일 때
 			int[] ans_0 = {1,6};
 			return ans_0;
-		}else {
-			int count = 0;
-			int count_0 = 0;
+		}else { // 아닐 때
+			int count = 0; // 일치하는 개수
+			int count_0 = 0; // 0의 개수
 			for(int i = 0; i < 6; i++) {
 				for(int j = 0; j < 6; j++) {
-					if(lottos[i] == win_nums[i]) {
+					if(lottos[i] == win_nums[j]) {
 						count++;
 					}
 				}
@@ -31,7 +31,9 @@ class Solution {
 		}
     }
     public static int rank(int n) {
-		if(n>=6) {n = 6;}
+		if(n>=6) {
+			n = 6;
+		}
 		switch (n) {
 		case 6:
 			return 1; 
