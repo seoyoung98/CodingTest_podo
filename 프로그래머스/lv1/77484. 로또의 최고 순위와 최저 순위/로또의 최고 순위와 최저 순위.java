b4e071fc46101a670238df4f9b04d7767b32d18c
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-09-21 23:44:18 +0900

    Update 로또의 최고 순위와 최저 순위.java

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,6 +1,50 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
-        return answer;
+        int[] answer = new int[2];
+		int min = 60;
+        
+		for(int i= 0; i < 6; i++) {
+			if(lottos[i] < min) {
+				min = lottos[i]; // 최소값 찾기
+			}
+		}
+		if(min == 0) {
+			int[] ans_0 = {1,6};
+			return ans_0;
+		}else {
+			int count = 0;
+			int count_0 = 0;
+			for(int i = 0; i < 6; i++) {
+				for(int j = 0; j < 6; j++) {
+					if(lottos[i] == win_nums[i]) {
+						count++;
+					}
+				}
+				if(lottos[i] == 0) {
+					count_0++;
+				}
+			}
+			int max_num = count + count_0;
+			answer[0] = rank(max_num);
+			answer[1] = rank(count);
+			return answer;
+		}
     }
+    public static int rank(int n) {
+		if(n>=6) {n = 6;}
+		switch (n) {
+		case 6:
+			return 1; 
+		case 5:
+			return 2; 
+		case 4:
+			return 3; 
+		case 3:
+			return 4; 
+		case 2:
+			return 5; 
+		default:
+			return 6;
+		}
+	}
 }
