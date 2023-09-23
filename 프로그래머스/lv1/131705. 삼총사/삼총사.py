bee8cb7df0b3d0856332093afe2853f594ef6778
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-09-23 15:33:13 +0900

    Update 삼총사.py

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -3,6 +3,21 @@
 학생 3명의 정수 번호를 더했을 때 0이면 3명은 삼총사
 삼총사가 될 경우의 수를 출력
 '''
+'''
+경우의 수를 사용할 수 있는 라이브러리
+1. 순열 (Permutation)
+순서는 있으나 중복 없는 모든 경우의 수 
+n = list(permutations(배열, 개수))
+2. 조합 (Combination)
+순서는 없고 중복 없는 모든 경우의 수
+n = list(combinations(배열, 개수))
+3. 중복 순열 (Permutation with repetition)
+중복을 허용하는 순열, 같은 값이 나오는 경우를 말한다.
+n = list(product(배열, repeat = 개수))
+4. 중복 조합 (Combination with repetition)
+중복 포함, 순서를 고려하지 않는 경우
+n = list(combinations_with_replacement(배열, 개수))
+'''
 from itertools import *
 def solution(number):
     answer = 0
