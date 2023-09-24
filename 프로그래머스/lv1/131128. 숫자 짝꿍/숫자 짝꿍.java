19db0649397fa374073e363c2f97e5d42c245088
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-09-24 20:02:31 +0900

    Update 숫자 짝꿍.java

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -4,19 +4,19 @@ class Solution {
         int[] x_lst = new int[10];
         int[] y_lst = new int[10];
         
-        for(int i = 0; i < X.length(); i++){
+        for(int i = 0; i < X.length(); i++){ // 해당 숫자 인덱스 ++
             x_lst[(int)(X.charAt(i)-'0')]++;
         }
         for(int i = 0; i < Y.length(); i++){
             y_lst[(int)(Y.charAt(i)-'0')]++;
         }
         
-        for(int i = 9; i >= 0; i--){
+        for(int i = 9; i >= 0; i--){ 
             int n = Math.min(x_lst[i],y_lst[i]);
             answer += Integer.toString(i).repeat(n);    
         }
         int n_0 = Math.min(x_lst[0],y_lst[0]);
-        if (answer.length() == 0) return "-1";
+        if (answer.length() == 0) return "-1"; // 일치하는 것이 없다면
         else if (answer.length() == n_0) return "0";
         else return answer;
     }
