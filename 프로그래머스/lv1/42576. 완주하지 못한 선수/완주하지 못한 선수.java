e7625aca6339f38aa137467a251978125eca17bd
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-09-25 14:01:44 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-	    String answer = "";
+	String answer = "";
 	    
         Arrays.sort(participant);
 	    Arrays.sort(completion);
-	    for(int i = 0; i < completion.length; i++){
+        // 두 리스트를 비교하는데 리스트의 길이가 다르다면 작은 길이를 기준으로 비교한다.
+	    for(int i = 0; i < completion.length; i++){ 
             if(!participant[i].equals(completion[i])) {
                 answer += participant[i];
                 break;  
@@ -18,5 +19,5 @@ class Solution {
         else{
             return answer;
         }
-	}
+    }
 }
