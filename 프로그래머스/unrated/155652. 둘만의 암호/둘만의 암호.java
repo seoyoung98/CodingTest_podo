cfe6b01edbf26c82a4a7bd2c7dfce454cfca372f
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-02 00:46:09 +0900

    [unrated] Title: 둘만의 암호, Time: 1.79 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+		
+		List<String> english = new ArrayList<>();
+		for (char i = 'a'; i <= 'z'; i++) {
+			english.add(String.valueOf(i));
+		}
+		
+		String[] s_list = s.split("");
+		String[] skip_list = skip.split("");
+		
+		
+		int minus_length = english.size() - skip.length();
+		
+		// skip 지우기 
+		for (int i = 0; i < skip_list.length; i++) {
+			int idx = english.indexOf(skip_list[i]); // skip와 일치하는 문자의 인덱스를 찾아서 
+			english.remove(idx);
+		}
+		
+		for (int i = 0; i < s_list.length; i++) {
+			int idx = english.indexOf(s_list[i]);
+			answer += english.get((idx + index)%minus_length);
+		}
+		
+		return answer;
+    }
+}
