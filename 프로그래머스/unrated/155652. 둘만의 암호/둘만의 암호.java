aefceeaa5902137eec76d732e3343c3b36fda654
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-02 21:26:31 +0900

    Update 둘만의 암호.java

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -3,29 +3,31 @@ class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
 		
-		List<String> english = new ArrayList<>();
-		for (char i = 'a'; i <= 'z'; i++) {
-			english.add(String.valueOf(i));
-		}
-	        // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	List<String> english = new ArrayList<>();
+	for (char i = 'a'; i <= 'z'; i++) {
+		english.add(String.valueOf(i));
+	}
+	// a b c d e f g h i z k l m n o p q r s t u v w x y z
+	// Arrays.asList로 만든 리스트는 변경 불가! => 강태연씨 
 		
-		String[] s_list = s.split("");
-		String[] skip_list = skip.split("");
+	String[] s_list = s.split("");
+	String[] skip_list = skip.split("");
 		
+	int minus_length = english.size() - skip.length();
 		
-		int minus_length = english.size() - skip.length();
+	// skip 지우기 
+	// skip w b q d
+	// a c e f g h i z k l m n o p r s t u v x y z
+	for (int i = 0; i < skip_list.length; i++) {
+		int idx = english.indexOf(skip_list[i]); // skip와 일치하는 문자의 인덱스를 찾아서 
+		english.remove(idx);
+	}
 		
-		// skip 지우기 
-		for (int i = 0; i < skip_list.length; i++) {
-			int idx = english.indexOf(skip_list[i]); // skip와 일치하는 문자의 인덱스를 찾아서 
-			english.remove(idx);
-		}
+	for (int i = 0; i < s_list.length; i++) {
+		int idx = english.indexOf(s_list[i]);
+		answer += english.get((idx + index)%minus_length);
+	}
 		
-		for (int i = 0; i < s_list.length; i++) {
-			int idx = english.indexOf(s_list[i]);
-			answer += english.get((idx + index)%minus_length);
-		}
-		
-		return answer;
+	return answer;
     }
 }
