615e9c9cac808df181ee2730f1674b13d097b663
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-05 23:54:05 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,20 @@
+'''
+1대1 대결, 대결마다 음식의 종류와 양이 바뀐다.
+음식을 일렬로 배치한 후 한 명은 젤 왼쪽부터, 한 명은 젤 오른쪽 부터 먹는다.
+중앙 물, 제일 먼저 먹는 선수가 승리
+양 쪽 양과 종류가 같아야 하고 첫 시작은 칼로리가 낮은 음식부터
+대회를 위한 음식의 배치를 나타내는 문자열을 return
+'''
+
+def solution(food):
+    left_list = []
+    total_list = []
+    for i in range(1,len(food)):
+        for j in range(food[i]//2):
+            left_list.append(str(i))
+    
+    right_list = left_list[::-1]
+    total_list = left_list
+    total_list.append('0')
+    total_list = total_list + right_list
+    return ''.join(total_list)
