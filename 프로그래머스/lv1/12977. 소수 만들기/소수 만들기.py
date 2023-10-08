1d67bf7aee8b736a0a48facaa461cda34f4e3c01
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-08 15:47:39 +0900

    Update 소수 만들기.py

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -2,6 +2,7 @@ from itertools import combinations
 
 def condition(n):  # 소수 판별
     b = 0
+#    for i in range(2, n): # 이 줄을 버리고 아래줄로 바꿔주니까 시간이 미친듯이 줄었따
     for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             b += 1
