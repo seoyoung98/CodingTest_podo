403f83d803df962193192445bc0fa9db75b2dd01
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-10 01:11:01 +0900

    Update 햄버거 만들기.py

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -7,5 +7,8 @@ def solution(ingredient):
         stack.append(ingredient[i])
         if stack[-4:] == ham:
             del stack[-4:]
+            # del이 속도가 아주 살짝 빠른 것 같다ㅏ
+            # for j in range(4):
+                # stack.pop()
             answer += 1
     return answer
