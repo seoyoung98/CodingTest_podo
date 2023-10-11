160655c0a1f350cf599155ac720455e27550d51f
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-11 14:48:02 +0900

    [level 2] Title: 올바른 괄호, Time: 21.64 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = True
+    stack= []
+    for i in range(len(s)):
+        stack.append(s[i])
+        if(stack[-2:] == ['(', ')']):
+            stack.pop()
+            stack.pop()
+    if(len(stack) == 0):
+        return True
+    else:
+        return False
