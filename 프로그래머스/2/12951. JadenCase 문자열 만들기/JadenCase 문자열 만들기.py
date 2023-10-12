dc168a8ec2179e1bf01760bcbb90709656148bf1
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-12 16:32:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,29 @@
+# def solution(s):
+#     result = []
+#     s_list = s.split(" ")
+    
+#     idx = 0
+#     for i in range(len(s)):
+#         if(s[i] == " "):
+#             if(s[idx].isalpha()):
+#                 result.append(s[idx:i].capitalize())
+#             else:
+#                 result.append(s[idx:i])
+#             idx = i+1
+#             result.append(" ")
+    
+#     if(s_list[-1][0].isalpha()):
+#         result.append(s_list[-1].capitalize())
+#     else:
+#         result.append(s_list[l-1])
+        
+#     return ''.join(result)
+
+def solution(s):
+    word=s.split(' ') # 공백을 기준으로 나눠주면 공백 순서에 ''가 들어간 배열이 생성된다.
+    for i in range(len(word)):
+        if word[i] and word[i][0].isalpha(): # 공백이 아니고 알파벳으로 시작하면 
+            word[i]=word[i].capitalize()# 
+        else:
+            word[i]=word[i].lower()
+    return ' '.join(word)
