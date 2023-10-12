38ef716d7d796060ca43299ac1560c3982a8234c
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-12 18:41:31 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.10 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,25 +1,21 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Solution {
-    public int[] solution(int[] numbers) {
-		int len = 0;
-		int cnt = 0;
-		int cnt2 = 0;
-		Arrays.sort(numbers);
-		len = numbers[numbers.length - 1] + numbers[numbers.length - 2] + 1;
-		int[] n = new int[len];
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = i + 1; j < numbers.length; j++) {
-				n[numbers[i] + numbers[j]]++;
-			}
-		}
-		for (int i = 0; i < len; i++) {
-			if (n[i] != 0) cnt++;
-		}
-		System.out.println(cnt);
-		int[] a = new int[cnt];
-		for (int i = 0; i < len; i++) {
-			if (n[i] != 0) a[cnt2++] = i;
-		}
-        return a;
+    public ArrayList<Integer> solution(int[] numbers) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length; i++) {
+        	for(int j = 0; j < numbers.length; j++) {
+        		if(i != j) {
+        			if(!answer.contains(numbers[i] + numbers[j])) {
+        				answer.add(numbers[i] + numbers[j]);
+        			}
+        		}
+        	}
+        }
+        Collections.sort(answer);
+        
+        return answer;
     }
 }
