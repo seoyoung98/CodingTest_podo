04eed74414330a4503ed6f6e44b649d2e8b54871
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-14 14:59:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -6,9 +6,17 @@ def solution(N, A, B):
     small = num[0]
     big = num[1]
 
-    while not (small % 2 == 1 and big % 2 == 0 and small+1 == big):
-        small = small // 2 + small % 2
-        big = big // 2 + big % 2
+    while True:
+        if(small % 2 == 1 and big % 2 == 0 and small+1 == big):
+            break
+        if small % 2 == 0: # 짝수면 
+            small = small // 2
+        else: # 홀수면
+            small = small // 2 + 1
+        if big % 2 == 0:
+            big = big // 2
+        else:
+            big = big // 2 + 1
         answer += 1
 
     return answer + 1
