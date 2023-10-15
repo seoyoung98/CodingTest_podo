f992f96c117f38d6585e0f052c244d81484ee52e
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-15 23:35:24 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for(int i = 0; i < photo.length; i++) {
+        	int y_sum = 0;
+        	for(int j = 0; j < photo[i].length; j++) {
+        		for(int k = 0; k < name.length; k++) {
+        			if(name[k].equals(photo[i][j])) {
+        				y_sum += yearning[k];
+        			}
+        		}
+        	}
+        	answer[i] = y_sum;
+        }
+        return answer;
+    }
+}
