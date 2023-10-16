ccb9c022e7cef7620f5fdca547c13549e48f0eb8
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-16 10:21:44 +0900

    [level 2] Title: 귤 고르기, Time: 40.56 ms, Memory: 25.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,21 @@
+def solution(k, tangerine):
+    sum = 0
+    answer = 0
+    tangerine_dic = {}
+    for i in range(len(tangerine)):
+        if (tangerine[i] not in tangerine_dic):
+            tangerine_dic[tangerine[i]] = 1
+        else:
+            tangerine_dic[tangerine[i]] += 1
+    tangerine_dic = dict(
+        sorted(tangerine_dic.items(), key=lambda x: x[1], reverse=True))
+
+    # 여기까지 정렬 완료
+    for key, value in tangerine_dic.items():
+        if (sum >= k):
+            break
+        else:
+            sum += value
+            answer += 1
+
+    return answer
