9ec1bfd3ce7c6eec51500112401276c38c761baa
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-19 01:43:01 +0900

    [level 1] Title: 모의고사, Time: 0.65 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.25 ms
+메모리: 74.9 MB, 시간: 0.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 8:53:21
+2023년 10월 4일 1:42:59
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int[] answers) {
+        // 쉣,, 자바 왜 배열 곱하기 안되녀,,ㅠㅠ
+		ArrayList<Integer> answer = new ArrayList<>();
+        
+		int[] student1 = { 1, 2, 3, 4, 5 }; // 5개
+		int[] student2 = { 2, 1, 2, 3, 2, 4, 2, 5 }; // 8개
+		int[] student3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }; // 10개
+
+		int[] students = { 0, 0, 0 };
+
+		for (int i = 0; i < answers.length; i++) {
+			if (answers[i] == student1[i % 5]) {
+				students[0]++;
+			}if (answers[i] == student2[i % 8]) {
+				students[1]++;
+			}if (answers[i] == student3[i % 10]) {
+				students[2]++;
+			}
+		}
+		// System.out.println(Arrays.toString(students));
+		// 최고 개수
+		int maxTest = Math.max(students[0], Math.max(students[1], students[2]));
+		//System.out.println(maxTest);
+		for (int i = 0; i < 3; i++) {
+			if (students[i] == maxTest) {
+				answer.add(i + 1);
+			}
+		}
+		// 0빼기
+		answer.remove(Integer.valueOf(0));
+
+		return answer;
+    }
+}
