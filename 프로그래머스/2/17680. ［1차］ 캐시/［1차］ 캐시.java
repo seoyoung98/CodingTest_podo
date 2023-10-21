d3c92496c68968a4b5ad600ee0d9c6ee4818502a
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-10-21 13:56:25 +0900

    Update ［1차］ 캐시.java

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,4 +1,24 @@
 import java.util.*;
+/*
+ * 지도에서 도시 이름을 검색하면 관련 맛집을 보여줌
+ * 서비스 오픈 전 각 로직에 대한 성능 측정
+ * DB 캐시를 적용할 때 캐시 크기에 따른 실행 시간 측정 프로그램 작성
+ * 
+ * cacheSize -> 캐시 크기
+ * cities -> 도시 이름 배열
+ * 
+ * 입력된 배열을 순서대로 처리할 시 총 실행시간 출력
+ * 
+ * 캐시 크기만큼만 유지되도록
+ * 미쳤다!! 찢었다!
+ */
+/*
+ * LRU(Least Recently Used) 알고리즘
+ * 맨 앞이 가장 최근에 쓰인 작업
+ * 맨 뒤는 가장 오랫동안 쓰이지 않은 작업
+ * cache miss -> 해야 할 작업이 없는 상태 5초
+ * cache hit -> 해야 할 작업이 있는 상태 1초
+ */
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
