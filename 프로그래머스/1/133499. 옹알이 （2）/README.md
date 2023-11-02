a1b16b9c1c7cbac2e31430a9aeaac94e1b542319
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-11-02 17:48:17 +0900

    [level 1] Title: 옹알이 (2), Time: 2.35 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 2.00 ms
+메모리: 76 MB, 시간: 2.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 17:38:30
+2023년 11월 4일 17:48:14
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -7,6 +7,11 @@ class Solution {
 			for (int j = 0; j < words.length; j++) {
 				if (babbling[i].contains(words[j]) && !babbling[i].contains(words[j] + words[j])) {
                     babbling[i] = babbling[i].replace(words[j], " ");
+                    // replaceFirst를 사용하지 못하는 이유!!
+                    // 예제 2번에서 yemawoo를 보세요
+                    // words에서는 ma 보다 woo가 먼저 나오죠?
+                    // 근데 replaceFirst를 사용하면 woo가 지워지지가 않습니다.
+                    // 뒤에 위치하는 woo를 지우기 위해서는 그냥 replace를 써줘야 합니다!!!
                 }
 			}
 			babbling[i] = babbling[i].replace(" ", "");
