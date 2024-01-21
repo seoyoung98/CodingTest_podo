eee9129c28905952a75fdecda9fa499739bfee03
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-01-21 11:15:23 +0900

    Update 타겟 넘버.java

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,21 @@
 class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
+    int answer;
+    // 모든 경우의 수를 다 돈다.!!
+    public int solution(int[] numbers, int target){
+        answer = 0;
+        dfs(0,0,numbers,target);
         return answer;
+    }
+
+    public void dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.length){
+            if(sum == target) answer++;
+            return;
+        }
+        //System.out.println("depth ===> " + depth);
+        //System.out.println("n last + " + depth);
+        //System.out.println("sum => " + sum);
+        dfs(depth+1, sum + numbers[depth], numbers, target);
+        dfs(depth+1, sum - numbers[depth], numbers, target);
     }
 }
