a6739cc2ac839422dfca51f66e01aaa17b9b0d2b
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-01-24 23:37:01 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,6 @@
 class Solution {
     int answer;
-    // 모든 경우의 수를 다 돈다.!!
+
     public int solution(int[] numbers, int target){
         answer = 0;
         dfs(0,0,numbers,target);
@@ -9,19 +9,10 @@ class Solution {
 
     public void dfs(int depth, int sum, int[] numbers, int target){
         if(depth == numbers.length){
-            System.out.println("으악");
-            System.out.println("sumsum" + sum);
-            if(sum == target) {
-                System.out.println("성공");
-                answer++;
-            }
+            if(sum == target) answer++;
             return;
         }
-        System.out.println("depth ===> " + depth);
-        System.out.println("sum => " + sum);
         dfs(depth+1, sum + numbers[depth], numbers, target);
-        System.out.println("오나");
         dfs(depth+1, sum - numbers[depth], numbers, target);
-        System.out.println("last 팡");
     }
 }
