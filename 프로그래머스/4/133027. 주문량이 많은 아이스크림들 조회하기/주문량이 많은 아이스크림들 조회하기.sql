f868e4f574082b34353e3ca8de172bba72da237b
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-01-29 15:07:04 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,11 +1,30 @@
 -- 코드를 입력하세요
-select flavor from(
-select  s.flavor, sum(total_order) as sum
-from (SELECT *
-    from first_half f union 
-    select *
-    from july j) s
-group by s.flavor
-order by sum desc)
+-- select flavor from(
+-- select  s.flavor, sum(total_order) as sum
+-- from (SELECT *
+--     from first_half f union 
+--     select *
+--     from july j) s
+-- group by s.flavor
+-- order by sum desc)
+-- where rownum <= 3
+
+select flavor
+from (select f.flavor, sum(f.total_order) + sum(j.total_order) as sum
+        from first_half f join july j
+        on f.flavor = j.flavor
+        group by f.flavor
+        order by sum desc
+     )
 where rownum <= 3
-    
+
+-- SELECT flavor
+-- FROM (
+--     SELECT f.flavor, SUM(f.total_order) + COALESCE(SUM(j.total_order), 0) AS sum
+--     FROM first_half f
+--     LEFT JOIN july j ON f.flavor = j.flavor
+--     GROUP BY f.flavor
+--     ORDER BY sum DESC
+-- ) subquery
+-- WHERE ROWNUM <= 3;
+     
