ab26a8ec6a166f2984f326290547db72f5c8b0b0
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-13 20:32:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,29 @@
+def solution(n):
+    answer = num_3(n)
+    return int(answer[::-1],3)
+
+def num_3(n):
+    answer = ""
+    while n > 0:
+        n, mod = divmod(n, 3)
+        answer += str(mod)
+    return answer[::-1] # 뒤집은 것 까지가 3진법 만들기
+
+
+# def solution(n):
+#     n = str(n) # 문자로 바꾸기
+#     answer = num_3(n) # 3진법으로 바꾸기
+#     reverse_answer = "" # 빈 문자열 만들기
+#     for i in range(len(answer)-1,-1,-1): # 3진법 거꾸로 돌리기
+#         reverse_answer += answer[i] # 집어넣기
+#     answer_10 = int(reverse_answer,3) # 다시 10진법으로 바꾸기
+#     return answer_10
+
+# def num_3(n):
+#     n = int(n)
+#     answer = ""
+#     while n > 0:
+#         n, mod = divmod(n,3)
+#         answer += str(mod)
+#     return answer[::-1]
+    
