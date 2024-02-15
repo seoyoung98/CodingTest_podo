3b8272e1ab808cc5d82e881ec344d1e92fb95d81
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-15 21:16:27 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,17 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        list = array[commands[i][0]-1:commands[i][1]]
+        list.sort()
+        answer.append(list[commands[i][2]-1])
+    return answer
+
+# def solution(array, commands):
+#     answer = []
+#     for i in range(len(commands)):
+#         first_num = commands[i][0] - 1
+#         second_num = commands[i][1]
+#         list = array[first_num : second_num]
+#         list.sort()
+#         answer.append(list[commands[i][2]-1])
+#     return answer
