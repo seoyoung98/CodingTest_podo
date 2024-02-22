b90950b7d3723d0473fc6d8e4676900b3598e042
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-22 23:45:02 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 12.51 ms
+메모리: 10.2 MB, 시간: 1.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 12:48:29
+2024년 02월 22일 23:44:59
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,10 +1,20 @@
 def solution(s):
-    count_step = 0
-    count_zero = 0
+    count = [0,0]
     while s != "1":
-        count_zero += s.count("0")  # 0의 개수 업데이트
-        s = s.replace("0", "")  # 0 제거
-        count_step += 1
+        count[1] += s.count("0")
+        s = s.replace("0","")
+        count[0] += 1
         s = str(bin(len(s))[2:])
+    return count
+
+        
+# def solution(s):
+#     count_step = 0
+#     count_zero = 0
+#     while s != "1":
+#         count_zero += s.count("0")  # 0의 개수 업데이트
+#         s = s.replace("0", "")  # 0 제거
+#         count_step += 1
+#         s = str(bin(len(s))[2:])
             
-    return [count_step, count_zero]
+#     return [count_step, count_zero]
