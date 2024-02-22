11d29a37e6e5bca43d153e9543d56c345a232a97
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-22 23:59:22 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911#qna) 
 
 ### 성능 요약
 
-메모리: 52.7 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 13:5:31
+2024년 02월 22일 23:59:18
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,17 @@
 def solution(n):
-    bin_n = int(bin(n)[2:])
-    print(bin_n)
-    for i in range(n+1, 1000000):
-        next_big = int(bin(i)[2:])
-        if (str(next_big).count("1") == str(bin_n).count("1")):
-            return i
+    binary_n = int(bin(n)[2:])
+    b_count = str(binary_n).count("1")
+    i = 1
+    while True:
+        next = int(bin(n+i)[2:])
+        if str(next).count("1") == b_count:
+            return n+i
+        i += 1
+
+# def solution(n):
+#     bin_n = int(bin(n)[2:])
+#     print(bin_n)
+#     for i in range(n+1, 1000000):
+#         next_big = int(bin(i)[2:])
+#         if (str(next_big).count("1") == str(bin_n).count("1")):
+#             return i
