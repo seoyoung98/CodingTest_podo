cc4b3c1e6fb16fb62c125b39ed304e5c28ca4d87
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-22 23:20:12 +0900

    [level 2] Title: 올바른 괄호, Time: 21.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,25 @@
 def solution(s):
     answer = True
-    stack= []
+    stack = []
     for i in range(len(s)):
         stack.append(s[i])
-        if(stack[-2:] == ['(', ')']):
+        if stack[-2:] == ['(',')']:
             stack.pop()
             stack.pop()
-    if(len(stack) == 0):
-        return True
-    else:
-        return False
+            
+    if len(stack) > 0:
+        answer = False
+    return answer
+
+# def solution(s):
+#     answer = True
+#     stack= []
+#     for i in range(len(s)):
+#         stack.append(s[i])
+#         if(stack[-2:] == ['(', ')']):
+#             stack.pop()
+#             stack.pop()
+#     if(len(stack) == 0):
+#         return True
+#     else:
+#         return False
