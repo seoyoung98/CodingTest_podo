3c5714b172e13aca47ca80b3f5409ce79f77442c
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-24 00:15:30 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,9 +1,9 @@
 def solution(n):
     move = 1
-    while(n > 1):
-        if(n%2==0):
+    while (n > 1):
+        if n % 2 == 0:
             n = n/2
         else:
-            n = n-1
+            n = n - 1
             move += 1
     return move
