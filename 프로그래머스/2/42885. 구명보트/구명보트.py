78d3666e1340eb3ff3746e5b13083acc18fcdac7
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-24 00:39:45 +0900

    [level 2] Title: 구명보트, Time: 11.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -6,23 +6,57 @@
 from collections import deque
 # 투포인터에 관련된 문제다/!!
 
-
 def solution(people, limit):
-    answer = 0
     people.sort()
-    de_people = deque()
-    for p in people:
-        de_people.append(p)
-
+    de_people = deque(people)
+    count = 0
     while len(de_people) > 1:
-        if (de_people[0] + de_people[-1] > limit):
-            answer += 1
-            de_people.pop()  # 제일 작은 친구와 제일 큰 친구가 한도를 넘었으니까 마지막 친구만 빼자
-        else:  # 안 넘으면 둘다 빼자
-            answer += 1
+        if de_people[0] + de_people[-1] > limit:
+            count += 1
+            de_people.pop()
+        else:
+            count += 1
             de_people.pop()
             de_people.popleft()
-
     if len(de_people) == 1:
-        answer += 1
-    return answer
+        count += 1
+    
+    return count        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(people, limit):
+#     answer = 0
+#     people.sort()
+#     de_people = deque()
+#     for p in people:
+#         de_people.append(p)
+
+#     while len(de_people) > 1:
+#         if (de_people[0] + de_people[-1] > limit):
+#             answer += 1
+#             de_people.pop()  # 제일 작은 친구와 제일 큰 친구가 한도를 넘었으니까 마지막 친구만 빼자
+#         else:  # 안 넘으면 둘다 빼자
+#             answer += 1
+#             de_people.pop()
+#             de_people.popleft()
+
+#     if len(de_people) == 1:
+#         answer += 1
+#     return answer
