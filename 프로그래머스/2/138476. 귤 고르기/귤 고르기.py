43c72c3619e81fad7d33ef06924dbd011eb54da8
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-02-25 21:10:20 +0900

    [level 2] Title: 귤 고르기, Time: 84.28 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,21 +1,36 @@
 def solution(k, tangerine):
-    sum = 0
-    answer = 0
-    tangerine_dic = {}
+    count = 0
+    box = [0] * (max(tangerine) + 1)
     for i in range(len(tangerine)):
-        if (tangerine[i] not in tangerine_dic):
-            tangerine_dic[tangerine[i]] = 1
-        else:
-            tangerine_dic[tangerine[i]] += 1
-    tangerine_dic = dict(
-        sorted(tangerine_dic.items(), key=lambda x: x[1], reverse=True))
+        box[tangerine[i]] += 1
+    box.sort(reverse=True)
+    i = 0
+    while (k > 0):
+        k -= box[i]
+        i += 1
+        count += 1
+    return count
+            
+    
 
-    # 여기까지 정렬 완료
-    for key, value in tangerine_dic.items():
-        if (sum >= k):
-            break
-        else:
-            sum += value
-            answer += 1
+# def solution(k, tangerine):
+#     sum = 0
+#     answer = 0
+#     tangerine_dic = {}
+#     for i in range(len(tangerine)):
+#         if (tangerine[i] not in tangerine_dic):
+#             tangerine_dic[tangerine[i]] = 1
+#         else:
+#             tangerine_dic[tangerine[i]] += 1
+#     tangerine_dic = dict(
+#         sorted(tangerine_dic.items(), key=lambda x: x[1], reverse=True))
 
-    return answer
+#     # 여기까지 정렬 완료
+#     for key, value in tangerine_dic.items():
+#         if (sum >= k):
+#             break
+#         else:
+#             sum += value
+#             answer += 1
+
+#     return answer
