188b7e1911fc31c21f5cf58b2e6b671658b74656
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-04-03 22:31:15 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 548 ms, Memory: 147040 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+# 키 => 단어
+# 값 => 개수 
+word_list = {}
+
+for i in range(n):
+    word = input()
+    if len(word) < m:
+        continue
+
+    if word in word_list:
+        word_list[word][0] += 1
+    else:
+        word_list[word] = [1, len(word), word]
+
+# print("정렬 전 : ", word_list)
+
+word_list = sorted(word_list.items(), key = lambda x:(-x[1][0], -x[1][1], x[1][2]))
+
+# print("정렬 후 : ",word_list)
+# print(word_list[0][1])
+
+for i in word_list:
+    print(i[0])
