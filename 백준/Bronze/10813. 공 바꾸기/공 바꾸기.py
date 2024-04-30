04d1f25706619fa17968e3ed4df5f2bec06728aa
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-04-30 17:40:21 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+basket = []
+
+for i in range(n):
+    basket.append(i+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    basket[a-1], basket[b-1] = basket[b-1], basket[a-1]
+
+print(' '.join(map(str, basket)))
