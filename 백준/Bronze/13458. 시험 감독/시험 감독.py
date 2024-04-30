c1da6b7eb4c7112772edcb7255600f7e4b9ce10c
youngee <80522538+seoyoung98@users.noreply.github.com>
2024-04-30 20:33:41 +0900

    [Bronze II] Title: 시험 감독, Time: 712 ms, Memory: 146464 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,17 @@
+import math
+n = int(input())
+
+student = list(map(int, input().split()))
+
+b, c = map(int, input().split())
+
+teacher = 0
+
+for i in range(n):
+    student[i] -= b
+    teacher += 1
+    if student[i] > 0:
+        teacher += math.ceil(student[i] / c)
+        # print(student[i]/c, math.ceil(student[i] / c))
+
+print(teacher)
